bc230eabbc910b67330a1c4d11961c65ac051e3a
Dan Ross <ross9885@gmail.com>
2014-02-03 23:09:34 -0800

    Added a fourth awesome thing.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('gridspaceClientApp')
+.controller('MainCtrl', function ($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma',
+    'Foundation (wip)'
+  ];
+});
